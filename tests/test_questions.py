4e9c2cbfe4a18094c5ef7a56f37824a64af992b4
Anton Mozgovoy <anton_mozgovoy@inbox.ru>
2024-09-08 14:34:35 +0300

    заменил параметры на капслок в обеих параметризациях

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -9,9 +9,9 @@ class TestQuestionForm:
     @allure.title('Проверка списка с вопросами')
     @allure.description('Проверяем соответствие теста ответов на вопросы')
     @pytest.mark.parametrize(data.GetAnswear.parameter, data.GetAnswear.value)
-    def test_first_questions(self, driver, number, expected_answer):
+    def test_first_questions(self, driver, NUMBER, EXPECTED_ANSWER):
         question_form = MainPage(driver)
         question_form.kill_cookie()
-        question_form.find_question(number)
-        question_form.find_answer(number)
-        assert question_form.find_answer(number) == expected_answer
+        question_form.find_question(NUMBER)
+        question_form.find_answer(NUMBER)
+        assert question_form.find_answer(NUMBER) == EXPECTED_ANSWER
